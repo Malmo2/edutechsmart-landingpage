01d44c8d83152d68b60c6e223c877ff7ade4a892
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-10-05 12:59:07 +0200

    bug in merge

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-const menuBtn = document.querySelector(".menu-toggle");
-const navbar = document.querySelector(".navbar");
-menuBtn.addEventListener("click", () => {
-  navbar.classList.toggle("menu-open");
-});
-
-// Darkmode button 
-
-const themeCheck = document.getElementById('theme-toggle');
-const root = document.documentElement;
-
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (prefersDark) {
-  root.classList.add('dark-mode');
-  themeCheck.checked = true;
-}
-
-themeCheck.addEventListener('change', (e) => {
-  const isON = e.currentTarget.checked;
-  if (isON) {
-    root.classList.add('dark-mode');
-  } else {
-    root.classList.remove('dark-mode');
-  }
+const menuBtn = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
+menuBtn.addEventListener('click', () => {
+  navbar.classList.toggle('menu-open');
 });
 
 const btnHero = document.querySelector(".btn-hero");
