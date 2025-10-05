7d9e4c3e090e046e51e73ae4a5a26e282b71ccbd
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-10-05 13:07:03 +0200

    removed the prefered dark

--- darkmode.js
+++ darkmode.js
@@ -3,20 +3,11 @@
 const themeCheck = document.getElementById('theme-toggle');
 const root = document.documentElement;
 
+root.classList.remove('dark-mode');
+if (themeCheck) themeCheck.checked = false;
+
 if (themeCheck) {
-
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (prefersDark) {
-        root.classList.add('dark-mode');
-        themeCheck.checked = true;
-    }
-
     themeCheck.addEventListener('change', (e) => {
-        const isOn = e.currentTarget.checked;
-        if (isOn) {
-            root.classList.add('dark-mode');
-        } else {
-            root.classList.remove('dark-mode');
-        }
+        root.classList.toggle('dark-mode', e.currentTarget.checked);
     });
 }
